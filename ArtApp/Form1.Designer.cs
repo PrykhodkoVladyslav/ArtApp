6a54e02132862b1301a353888fe2efece27f6592
Владислав Приходько <3885vlad@gmail.com>
2022-12-07 12:25:22 +0200

    Добавьте файлы проекта.

---
+++ ArtApp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace ArtApp {
+	partial class Form1 {
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent() {
+			this.buttonUpdate = new System.Windows.Forms.Button();
+			this.textBoxPath = new System.Windows.Forms.TextBox();
+			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.comboBoxSource = new System.Windows.Forms.ComboBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonUpdate
+			// 
+			this.buttonUpdate.Location = new System.Drawing.Point(13, 13);
+			this.buttonUpdate.Name = "buttonUpdate";
+			this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+			this.buttonUpdate.TabIndex = 0;
+			this.buttonUpdate.Text = "Update";
+			this.buttonUpdate.UseVisualStyleBackColor = true;
+			this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+			// 
+			// textBoxPath
+			// 
+			this.textBoxPath.Location = new System.Drawing.Point(340, 12);
+			this.textBoxPath.Name = "textBoxPath";
+			this.textBoxPath.ReadOnly = true;
+			this.textBoxPath.Size = new System.Drawing.Size(641, 22);
+			this.textBoxPath.TabIndex = 2;
+			// 
+			// pictureBox
+			// 
+			this.pictureBox.Location = new System.Drawing.Point(10, 40);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(1062, 662);
+			this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox.TabIndex = 3;
+			this.pictureBox.TabStop = false;
+			// 
+			// comboBoxSource
+			// 
+			this.comboBoxSource.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxSource.FormattingEnabled = true;
+			this.comboBoxSource.Location = new System.Drawing.Point(95, 10);
+			this.comboBoxSource.Name = "comboBoxSource";
+			this.comboBoxSource.Size = new System.Drawing.Size(239, 24);
+			this.comboBoxSource.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1087, 717);
+			this.Controls.Add(this.comboBoxSource);
+			this.Controls.Add(this.pictureBox);
+			this.Controls.Add(this.textBoxPath);
+			this.Controls.Add(this.buttonUpdate);
+			this.Name = "Form1";
+			this.Text = "ArtApp";
+			this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonUpdate;
+		private System.Windows.Forms.TextBox textBoxPath;
+		private System.Windows.Forms.PictureBox pictureBox;
+		private System.Windows.Forms.ComboBox comboBoxSource;
+	}
+}
+
---
+++ ArtApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtApp {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+
+			comboBoxSource.Items.Add(@"https://api.waifu.pics/sfw/neko");
+			comboBoxSource.Items.Add(@"https://api.waifu.pics/nsfw/neko");
+			comboBoxSource.SelectedIndex = 0;
+		}
+
+		private void buttonUpdate_Click(object sender, EventArgs e) {
+			try {
+				LoadPicture();
+			}
+			catch (System.Net.WebException) {
+				MessageBox.Show("Помилка мережі. Не вдалося завантажити зображення", "Помилка", MessageBoxButtons.OK);
+			}
+			catch (Exception) {
+				MessageBox.Show("Під час завантаження зображення виникла невідома помилка", "Невідома помилка", MessageBoxButtons.OK);
+			}
+		}
+
+		private void LoadPicture() {
+			string url = comboBoxSource.SelectedItem.ToString();
+			HttpWebRequest myHttwebrequest = (HttpWebRequest)HttpWebRequest.Create(url);
+			HttpWebResponse myHttpWebresponse = (HttpWebResponse)myHttwebrequest.GetResponse();
+			StreamReader strm = new StreamReader(myHttpWebresponse.GetResponseStream());
+			string htmlText = strm.ReadToEnd();
+
+			string pattern = "{\"url\":\"(.*)\"}";
+
+			Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+			Match match = regex.Match(htmlText);
+
+			pictureBox.ImageLocation = textBoxPath.Text = match.Groups[1].Value;
+		}
+
+		private void Form1_SizeChanged(object sender, EventArgs e) {
+			pictureBox.Size = new Size(Width - pictureBox.Location.X * 2, Height - 30 - (pictureBox.Location.Y) * 2);
+		}
+	}
+
+	public class LinkHistory {
+
+	}
+}
